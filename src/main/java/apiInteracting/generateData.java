94766f417abf6cf856746324b71f5d3f07504bef
micksgav <153670740+micksgav@users.noreply.github.com>
2023-12-13 10:45:32 -0500

    Drug Interactions Update

--- src/main/java/apiInteracting/generateData.java
+++ src/main/java/apiInteracting/generateData.java
@@ -3,13 +3,23 @@ package apiInteracting;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 import org.json.*;
 
+import java.util.ArrayList;
+
 public class generateData {
+
+    public static ArrayList<String> prescriptionDrugs = new ArrayList<String>();
+
+
+    static int n = 0;
     static String[][] drugs;
+
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        generatePrescriptionList();
         getDrugs();
-        scraper();
     }
 
     public static void getDrugs() throws IOException {
@@ -17,7 +27,7 @@ public class generateData {
         //get drugs JSON
         String result = "";
         try {
-            Thread.sleep(500);
+            Thread.sleep(50);
             URL url = new URL("https://health-products.canada.ca/api/drug/drugproduct/?lang=en&type=json");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -41,9 +51,10 @@ public class generateData {
         try {
             JSONArray jsonArray = new JSONArray(result);
 
-
+            int j = 0;
             drugs = new String[jsonArray.length()][5];
-            for (int i = 0; i < jsonArray.length(); i++) {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(i + " / " + jsonArray.length());
                 JSONObject record = jsonArray.getJSONObject(i);
                 String className = record.optString("class_name");
                 String drugCode = record.optString("drug_code");
@@ -51,18 +62,23 @@ public class generateData {
                 String name = record.optString("brand_name");
                 String company = record.optString("company_name");
                 if ("Human".equals(className)) {
-                    String RXCUI = CODEtoRXCUI(drugCode);
-                    writer.write(DIN + "," + name + "," + RXCUI + "," + drugCode + "," + company + "\n");
-                    drugs[i] = new String[]{DIN, name, RXCUI, drugCode, company};
+                    if (check(drugCode)) {
+                        j++;
+                        String RXCUI = CODEtoRXCUI(drugCode);
+                        writer.write(DIN + "," + name + "," + RXCUI + "," + drugCode + "," + company + "\n");
+                        drugs[j] = new String[]{DIN, name, RXCUI, drugCode, company};
+                    }
+
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    writer.close();
+        writer.close();
     }
 
     public static String CODEtoRXCUI(String drugCode) throws IOException {
+
         String RXCUI = "";
         BufferedWriter errors = new BufferedWriter(new FileWriter("data\\drugs\\errors.txt"));
         try {
@@ -77,12 +93,12 @@ public class generateData {
             String line;
 
             while ((line = br.readLine()) != null) {
-                result = result+line;
+                result = result + line;
             }
 
 
             br.close();
-            result = result.substring(1, result.length()-1);
+            result = result.substring(1, result.length() - 1);
 
             JSONObject record = new JSONObject(result);
             String atc = record.optString("tc_atc_number");
@@ -99,7 +115,7 @@ public class generateData {
             String line2;
 
             while ((line2 = br2.readLine()) != null) {
-                result2 = result2+line2;
+                result2 = result2 + line2;
             }
 
             br2.close();
@@ -109,14 +125,19 @@ public class generateData {
                 RXCUI = result2.substring(result2.indexOf(":[") + 3, result2.indexOf("]") - 1);
             } catch (StringIndexOutOfBoundsException e) {
                 RXCUI = "0";
-                errors.write("RXCUI ZERO " + drugCode + " " + atc +"\n");
+                errors.write("ZERO " + drugCode + " " + atc + "\n");
             } catch (NumberFormatException e) {
                 RXCUI = result2.substring(result2.indexOf(":[") + 3, result2.indexOf(",") - 1);
             }
-            System.out.println(RXCUI + "Added");
+            System.out.println(RXCUI + " Added");
         } catch (Exception e) {
             System.out.println("There was an error. Error Code: " + e.getMessage());
-            errors.write(drugCode + " " + e.getMessage()+"\n");
+            errors.write("DC" + drugCode + " " + e.getMessage() + "\n");
+            n++;
+            if (n % 10 == 0) {
+                errors.close();
+                errors = new BufferedWriter(new FileWriter("data\\drugs\\errors.txt"));
+            }
         }
         errors.close();
         return RXCUI;
@@ -156,8 +177,64 @@ public class generateData {
             }
         }
 
+    }
+
+
+    public static boolean check(String drugCode) throws IOException {
+        for (String prescriptionDrug : prescriptionDrugs) {
+            if (drugCode.equals(prescriptionDrug)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+    public static void generatePrescriptionList() {
+        String result = "";
+        try {
+            Thread.sleep(50);
+            URL url = new URL("https://health-products.canada.ca/api/drug/schedule/?lang=en&type=json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            InputStream is = conn.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                result = result + line;
+            }
+
+            br.close();
+        } catch (Exception e) {
+            System.out.println("There was an error in getting the data from Health Canada. Error Code: " + e.getMessage());
+
         }
 
+        try {
+            JSONArray jsonArray = new JSONArray(result);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                System.out.println(i + " / " + jsonArray.length());
+                JSONObject record = jsonArray.getJSONObject(i);
+                String drugCode = record.optString("drug_code");
+                String type = record.optString("schedule_name");
+                if (type.compareTo("OTC") != 0) {
+                    if (type.compareTo("Homeopathic") != 0) {
+                        prescriptionDrugs.add(drugCode);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+
+
+
 
 
