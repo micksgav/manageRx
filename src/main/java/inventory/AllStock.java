7f387c3197618938f1c305d8e55853d3ce94fe1f
christinaw48 <363431354@gotvdsb.ca>
2023-12-24 08:53:01 -0500

    12/24/23

--- src/main/java/inventory/AllStock.java
+++ src/main/java/inventory/AllStock.java
@@ -170,8 +170,8 @@ public class AllStock {
 	* Dependencies: DrugStockLinkedList
 	* Throws/Exceptions: N/A
     */
-	public void shipmentArrival(int newStock, String arrivalDIN, String nameGen, String nameBrand, String classDrug) throws IOException {
-		updateStock(newStock, arrivalDIN, nameGen, nameBrand, classDrug);
+	public void shipmentArrival(int newStock, String arrivalDIN, String classDrug) throws IOException {
+		updateStock(newStock, arrivalDIN, classDrug);
 	} // end shipmentArrival	
 	
 	/** Method Name: updateStock
@@ -184,7 +184,7 @@ public class AllStock {
 	* Dependencies: DrugStock, Drug
 	* Throws/Exceptions: N/A
     */
-	public void updateStock(int newStock, String arrivalDIN, String nameGen, String nameBrand, String classDrug) throws IOException{
+	public void updateStock(int newStock, String arrivalDIN, String classDrug) throws IOException{
 		boolean isStocked = drugsList.checkStockDIN(arrivalDIN);
 		if(isStocked == false) { 		// if this is the inventory's first shipment of the drug
 			// update to interact with setThresholdButton in StockUI
